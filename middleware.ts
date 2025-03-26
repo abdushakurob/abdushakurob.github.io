d69b26f49bf7c62e647a1f27a6e18793ba24829c
abdu-shakur <bashirabdulshakur776@gmail.com>
2025-03-26 17:01:20 +0100

    Remove login page from middleware route matcher for improved URL handling and clarity in route configuration.

--- middleware.ts
+++ middleware.ts
@@ -37,8 +37,7 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - login (login page)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|login).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 } 
